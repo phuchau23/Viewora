9dc37e5498caaedc968d365a4d0cc8cd2f471fc9
Huynt1910 <huynt1910@gmail.com>
2025-08-06 12:25:27 +0700

    fix header web

--- app/(user)/user/snacks/[id]/page.tsx
+++ app/(user)/user/snacks/[id]/page.tsx
@@ -1,12 +1,16 @@
 import SnackDetailPage from "./components/SnackDetailPage";
 
-export default async function SnackPage(props: { params: Promise<{ id: string }> }) {
+export default async function SnackPage(props: {
+  params: Promise<{ id: string }>;
+}) {
   const params = await props.params;
   return <SnackDetailPage params={params} />;
 }
-export async function generateMetadata(props: { params: Promise<{ id: string }> }) {
+export async function generateMetadata(props: {
+  params: Promise<{ id: string }>;
+}) {
   const params = await props.params;
   return {
-    title: `Snack ${params.id}`,
+    title: `Viewora - Đặt vé xem phim`,
   };
 }
